f2c9676f1b812998369180794ae97219ff423e1a
teaching <teaching@tsuzukinoiMac.local>
2020-12-23 02:26:15 +0900

    created image_preprocessing

---
+++ api/tensorflow/image_preprocessing.py
@@ -0,0 +1,22 @@
+import base64
+import numpy as np
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
+
+
+def base64_to_numpy(base64_string):
+    decoded = base64.b64decode(base64_string)
+    img = Image.open(io.BytesIO(decoded))
+    numpy_img = np.array(img)
+
+    img_gray = numpy_img[:,:,3]
+    img_gray_norm = img_gray / 255.0
+
+    img_gray_norm_batch = np.expand_dims(img_gray_norm, 0)
+
+    plt.imshow(img_gray_norm_batch[0, :, :])
+    plt.colorbar()
+    plt.show()
+
+    return None
--- api/views.py
+++ api/views.py
@@ -3,10 +3,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+from .tensorflow import image_preprocessing
+
 
 @csrf_exempt
 def predict(request):
     body = json.loads(request.body)
+    base64_string = body["image"].split(",")[1]
+    numpy_image = image_preprocessing.base64_to_numpy(base64_string)
+
     print(body)
 
     return HttpResponse(status=200)
